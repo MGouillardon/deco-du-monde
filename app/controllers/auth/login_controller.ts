3d31b51afbd854b60388f09fae2af7cfcdc56524
Maxime <maxime.gouillardon@gmail.com>
2024-07-13 09:17:11 +0200

    feat: implement login controller

--- app/controllers/auth/login_controller.ts
+++ app/controllers/auth/login_controller.ts
@@ -1,4 +1,32 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import { loginValidator } from '#validators/auth/login'
+import type { HttpContext } from '@adonisjs/core/http'
+import { errors } from '@adonisjs/auth'
 
 export default class LoginController {
+  async render({ inertia }: HttpContext) {
+    return inertia.render('Admin/Login')
+  }
+
+  async login({ request, response, session, auth }: HttpContext) {
+    try {
+      const { email, password } = await request.validateUsing(loginValidator)
+      const user = await User.verifyCredentials(email, password)
+      await auth.use('web').login(user)
+
+      session.flash('success', 'You have been logged in successfully!')
+      return response.redirect().toRoute('inertia')
+    } catch (error) {
+      this.handleLoginError(error, session)
+      return response.redirect().back()
+    }
+  }
+
+  private handleLoginError(error: unknown, session: HttpContext['session']) {
+    if (error instanceof errors.E_INVALID_CREDENTIALS) {
+      session.flash('errors', 'Invalid email or password')
+    } else {
+      session.flash('errors', 'An unexpected error occurred. Please try again.')
+    }
+  }
 }
