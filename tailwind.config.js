9eca494ef9218e7e533df9114e0821c9181956f0
Maxime <maxime.gouillardon@gmail.com>
2024-07-13 07:59:36 +0200

    chore: add daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
   content: [
     './resources/**/*.edge',
     './resources/**/*.{js,ts,jsx,tsx,vue}',
     './inertia/**/*.{js,ts,jsx,tsx,vue}',
   ],
-
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['cupcake'],
+  },
 }
