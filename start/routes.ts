8f8854618455e003681dcfcf6cae7ee95b185ec4
Maxime <maxime.gouillardon@gmail.com>
2024-07-13 09:18:51 +0200

    feat: add login route & guest middleware

--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,14 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+const LoginController = () => import('#controllers/auth/login_controller')
 router.on('/').renderInertia('home', { version: 6 })
 
+router
+  .group(() => {
+    router.get('/login', [LoginController, 'render']).as('login')
+    router.post('/login', [LoginController, 'login'])
+  })
+  .prefix('admin')
+  .use(middleware.guest())
