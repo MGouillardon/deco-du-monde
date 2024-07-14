cc25e893917ead9c3e982ff6e9992314afd2af78
Maxime <maxime.gouillardon@gmail.com>
2024-07-14 20:50:48 +0200

    feat: implement users seeder

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -1,7 +1,23 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Role from '#models/role'
+import { UserFactory } from '#factories/user_factory'
 
 export default class extends BaseSeeder {
   async run() {
-    // Write your database queries inside the run method
+    const createUsersForRole = async (roleName: string, count: number) => {
+      const role = await Role.findBy('name', roleName)
+      if (!role) {
+        console.error(`Role '${roleName}' not found`)
+        return
+      }
+
+      await UserFactory.merge({ roleId: role.id }).createMany(count)
+    }
+
+    await createUsersForRole('photographe', 3)
+    await createUsersForRole('assistant-photographe', 6)
+    await createUsersForRole('décorateur', 3)
+    await createUsersForRole('assistant-decorateur', 6)
+    await createUsersForRole('chauffeur_assistant', 3)
   }
 }
