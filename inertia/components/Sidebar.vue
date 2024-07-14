d0c3d8e1fc91790603c10a0584b9ab08de5f8bd3
Maxime <maxime.gouillardon@gmail.com>
2024-07-14 20:14:18 +0200

    feat(sidebar): add active class to dahsboard link & write dropdown content

--- inertia/components/Sidebar.vue
+++ inertia/components/Sidebar.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HomeIcon from '@/components/icons/HomeIcon.vue'
+import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
@@ -8,22 +9,41 @@ import HomeIcon from '@/components/icons/HomeIcon.vue'
     <div
       class="menu bg-base-100 border-r-2 border-r-bg-base-200 text-base-content min-h-full w-80 p-0"
     >
-      <!-- Contenu de la sidebar ici -->
       <header class="navbar bg-base-100 border-b-2 border-b-base-200 pb-[2px] px-4 md:px-6 lg:px-8">
         <a class="btn btn-ghost text-xl">daisyUI</a>
       </header>
-      <ul class="menu bg-base-100 p-4">
-        <li><a><HomeIcon/>Dashboard</a></li>
+      <ul class="menu bg-base-100 p-4 md:p-6 lg:p-8 gap-4">
+        <li>
+          <Link href="/admin/dashboard" :class="{ active: $page.url === '/admin/dashboard' }"> <HomeIcon />Dashboard </Link>
+        </li>
+        <li><a>Calendar</a></li>
         <li>
           <details open>
-            <summary>Parent</summary>
+            <summary>Work</summary>
             <ul>
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
+              <li><a>Sets</a></li>
+              <li><a>Items</a></li>
             </ul>
           </details>
         </li>
-        <li><a>Item 3</a></li>
+        <li>
+          <details>
+            <summary>Reports</summary>
+            <ul>
+              <li><a>History</a></li>
+              <li><a>Analysis</a></li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Settings</summary>
+            <ul>
+              <li><a>Profile</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </details>
+        </li>
       </ul>
     </div>
   </aside>
