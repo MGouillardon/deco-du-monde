5135d78b68c57e42be6d103e0ae604b8b6cbeda7
Maxime <maxime.gouillardon@gmail.com>
2024-07-14 16:22:38 +0200

    feat: add AsideLayout component

---
+++ inertia/layouts/AsideLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Sidebar from '@/components/Sidebar.vue'
+</script>
+
+<template>
+  <div class="drawer lg:drawer-open">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col">
+      <slot></slot>
+    </div>
+    <Sidebar />
+  </div>
+</template>
