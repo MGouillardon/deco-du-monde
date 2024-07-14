179aeb9daaab8620b55aa7ffd427e5c343cc6bde
Maxime <maxime.gouillardon@gmail.com>
2024-07-14 15:47:35 +0200

    feat: add sidebar component

---
+++ inertia/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="drawer-side">
+    <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+    <ul class="menu bg-base-100 border-r-2 border-r-bg-base-200 text-base-content min-h-full w-80 p-4">
+      <!-- Contenu de la sidebar ici -->
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  </div>
+</template>
