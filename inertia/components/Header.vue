ebb59602beebf603291b8d90bd68a99778e1d46f
Maxime <maxime.gouillardon@gmail.com>
2024-07-14 14:38:54 +0200

    feat(header): implement header styling & content

--- inertia/components/Header.vue
+++ inertia/components/Header.vue
@@ -1,15 +1,43 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
+import { computed } from 'vue'
 
 const props = defineProps({
   user: String,
 })
+
+const userInitial = computed(() => {
+  return props.user
+    .split(' ')
+    .map((name) => name.charAt(0))
+    .join('')
+    .toUpperCase()
+})
 </script>
 
 <template>
-  <header>
-    <div class="navbar bg-base-200">
+  <header class="navbar bg-base-200 px-4 md:px-6 lg:px-8">
+    <div class="flex-1">
       <a class="btn btn-ghost text-xl">Déco du monde</a>
+    </div>
+    <div class="flex-none gap-2">
+      <div class="form-control">
+        <input type="text" placeholder="Search" class="input input-bordered input-sm w-24 md:w-auto" />
+      </div>
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-primary btn-circle">
+          <div class="w-8 h-8 rounded-full flex items-center justify-center">
+            <span>{{ userInitial }}</span>
+          </div>
+        </div>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+        >
+          <li><a>Profile</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
