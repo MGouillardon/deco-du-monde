67aebd1db58a41019f2f898a696b90bb2c388100
Maxime <maxime.gouillardon@gmail.com>
2024-07-14 09:34:06 +0200

    feat: add header component

---
+++ inertia/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+const props = defineProps({
+  user: String,
+})
+</script>
+
+<template>
+  <header>
+    <div class="navbar bg-base-200">
+      <a class="btn btn-ghost text-xl">Déco du monde</a>
+    </div>
+  </header>
+</template>
