587fc631970fa4194e5f54cc9456c208a495d6c8
Maxime <maxime.gouillardon@gmail.com>
2024-07-14 15:07:14 +0200

    feat: remove default layout to login view

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -1,8 +1,8 @@
-import '../css/app.css'
+import '@/css/app.css'
 import { createApp, h } from 'vue'
 import type { DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
-import DefaultLayout from '../layouts/DefaultLayout.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
@@ -14,7 +14,9 @@ createInertiaApp({
   resolve: (name) => {
     const pages = import.meta.glob<DefineComponent>('../pages/**/*.vue', { eager: true })
     let page = pages[`../pages/${name}.vue`]
-    page.default.layout = page.default.layout || DefaultLayout
+    if (name !== 'Admin/Login') {
+      page.default.layout = page.default.layout || DefaultLayout
+    }
     return page
   },
 
