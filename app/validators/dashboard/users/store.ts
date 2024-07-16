2a64541859057a2d0020b372e19d08b939772c84
Maxime <maxime.gouillardon@gmail.com>
2024-07-16 19:03:40 +0200

    feat: implement validator for storing a user

--- app/validators/dashboard/users/store.ts
+++ app/validators/dashboard/users/store.ts
@@ -1,1 +1,22 @@
 import vine from '@vinejs/vine'
+
+export const storeUserValidator = vine.compile(
+  vine.object({
+    fullName: vine
+      .string()
+      .minLength(4)
+      .unique(async (db, value) => {
+        const user = await db.from('users').where('full_name', value).first()
+        return !user
+      }),
+    email: vine
+      .string()
+      .email()
+      .unique(async (db, value) => {
+        const user = await db.from('users').where('email', value).first()
+        return !user
+      }),
+    password: vine.string().minLength(8),
+    roleId: vine.number().positive(),
+  })
+)
