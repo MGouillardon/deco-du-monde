0a717e84e0269d9ea7c6da627d99471706ec95d7
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2024-07-16 14:41:12 +0200

    feat(role_model): add a computed property + static property to improve role's names displayed

--- app/models/role.ts
+++ app/models/role.ts
@@ -1,4 +1,4 @@
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, computed } from '@adonisjs/lucid/orm'
 
 export default class Role extends BaseModel {
   @column({ isPrimary: true })
@@ -6,4 +6,23 @@ export default class Role extends BaseModel {
 
   @column()
   declare name: string
+
+  private static readonly displayNames: Record<string, string> = {
+    chauffeur_assistant: 'Chauffeur Assistant',
+    assistant_decorateur: 'Assistant Décorateur',
+    décorateur: 'Décorateur',
+    assistant_photographe: 'Assistant Photographe',
+    photographe: 'Photographe',
+  }
+
+  @computed()
+  get displayName() {
+    return (
+      Role.displayNames[this.name] ||
+      this.name
+        .split('_')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ')
+    )
+  }
 }
