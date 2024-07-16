98b0754b23d6d163ecd709026c540d5b9187e139
Maxime <maxime.gouillardon@gmail.com>
2024-07-16 21:29:25 +0200

    style(pagination): disable pointer event on link's hover & refactor pages computed poperty

--- inertia/components/UI/Pagination.vue
+++ inertia/components/UI/Pagination.vue
@@ -5,11 +5,7 @@ import { computed } from 'vue'
 const props = defineProps({ currentPage: Number, lastPage: Number })
 
 const pages = computed(() => {
-  const range = []
-  for (let i = 1; i <= props.lastPage; i++) {
-    range.push(i)
-  }
-  return range
+  return Array.from({ length: props.lastPage }, (_, i) => i + 1)
 })
 </script>
 
@@ -19,7 +15,10 @@ const pages = computed(() => {
       v-for="page in pages"
       :key="page"
       :href="`?page=${page}`"
-      :class="['join-item btn btn-sm', page === currentPage ? 'bg-neutral text-white' : 'bg-base-100']"
+      :class="[
+        'join-item btn btn-sm',
+        page === currentPage ? 'bg-neutral text-white pointer-events-none' : 'bg-base-100',
+      ]"
     >
       {{ page }}
     </Link>
