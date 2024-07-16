8c04f5d39b6770c03564ae0de769fe77036ddbe8
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2024-07-16 11:49:04 +0200

    style(sidebar): add z index to the drawer

--- inertia/components/Sidebar.vue
+++ inertia/components/Sidebar.vue
@@ -9,7 +9,7 @@ import DataIcon from '@/components/icons/DataIcon.vue'
 </script>
 
 <template>
-  <aside class="drawer-side">
+  <aside class="drawer-side z-20">
     <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
     <div
       class="menu bg-base-100 border-r-2 border-r-bg-base-200 text-base-content min-h-full w-80 p-0"
