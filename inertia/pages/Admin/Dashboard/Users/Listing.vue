fa44728ea8c470e64302da8daf70ad6c7ec4ea14
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2024-07-16 10:38:54 +0200

    feat(users_listing): add table layout to display all users

--- inertia/pages/Admin/Dashboard/Users/Listing.vue
+++ inertia/pages/Admin/Dashboard/Users/Listing.vue
@@ -1,5 +1,5 @@
 <script setup>
-const users = defineProps({
+const props = defineProps({
   users: {
     type: Array,
     required: true,
@@ -8,7 +8,32 @@ const users = defineProps({
 </script>
 
 <template>
-  <div>
-    {{ users }}
+  <div class="card bg-base-100 shadow-xl">
+    <div class="overflow-x-auto">
+      <table class="table">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, index) in props.users" :key="user.id">
+            <th>{{ index + 1 }}</th>
+            <td>{{ user.fullName }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.roleName }}</td>
+            <td class="flex gap-2">
+              <a class="btn btn-sm" href="#">Update</a>
+              <a class="btn btn-sm btn-error" href="#">Delete</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
