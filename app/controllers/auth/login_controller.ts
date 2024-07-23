2d2ef05b9f44acf063dbe4f6b9eecc5e13f71d60
Maxime <maxime.gouillardon@gmail.com>
2024-07-23 21:41:39 +0200

    feat: dependancy injection with email service

--- app/controllers/auth/login_controller.ts
+++ app/controllers/auth/login_controller.ts
@@ -2,9 +2,13 @@ import User from '#models/user'
 import { loginValidator } from '#validators/auth/login'
 import type { HttpContext } from '@adonisjs/core/http'
 import { errors } from '@adonisjs/auth'
-import mail from '@adonisjs/mail/services/main'
+import EmailService from '#services/email_service'
+import { inject } from '@adonisjs/core'
 
+@inject()
 export default class LoginController {
+  constructor(private emailService: EmailService) {}
+
   async render({ inertia }: HttpContext) {
     return inertia.render('Admin/Auth/Login')
   }
@@ -17,7 +21,7 @@ export default class LoginController {
 
       if (!user.isPasswordChanged) {
         const resetToken = await user.initiatePasswordReset()
-        await this.sendPasswordResetEmail(user, resetToken.token)
+        await this.emailService.sendPasswordResetEmail(user, resetToken.token)
         session.flash('info', 'Please check your email to reset your password.')
         return response.redirect().back()
       }
@@ -28,18 +32,6 @@ export default class LoginController {
       this.handleLoginError(error, session)
       return response.redirect().back()
     }
-  }
-
-  private async sendPasswordResetEmail(user: User, token: string) {
-    const resetUrl = `${process.env.APP_URL}/admin/password/reset/${token}`
-
-    await mail.send((message) => {
-      message
-        .from('noreply@example.com')
-        .to(user.email)
-        .subject('Reset Your Password')
-        .htmlView('emails/reset_password', { user, resetUrl })
-    })
   }
 
   private handleLoginError(error: unknown, session: HttpContext['session']) {
