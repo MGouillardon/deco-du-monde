1024b551c31ef3c70c1e88d660f3ba560cc8f1f6
Maxime <maxime.gouillardon@gmail.com>
2024-07-23 21:19:08 +0200

    refactor: errors handling to fix an issue when password is too short

--- inertia/components/Messages/ErrorFieldMessage.vue
+++ inertia/components/Messages/ErrorFieldMessage.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { computed } from 'vue'
+
 const props = defineProps({
-  error: Object,
+  error: {
+    type: [String, Array],
+    required: true,
+  },
+})
+
+const errorMessage = computed(() => {
+  if (typeof props.error === 'string') {
+    return props.error
+  }
+  return Array.isArray(props.error) ? props.error.join(', ') : ''
 })
 </script>
 
 <template>
   <p class="text-error text-xs m-0">
-    {{ error.join(', ') }}
+    {{ errorMessage }}
   </p>
 </template>
