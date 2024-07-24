f14c8c6ef9da04c5fbc7a7f55d044c0fc92fd60e
Maxime <maxime.gouillardon@gmail.com>
2024-07-24 19:19:07 +0200

    feat: add password reset service & implement logic to requestResetPassword method

--- app/controllers/admin/dashboard/profile_controller.ts
+++ app/controllers/admin/dashboard/profile_controller.ts
@@ -1,8 +1,13 @@
 import User from '#models/user'
 import { updateProfileValidator } from '#validators/dashboard/profile/update'
 import type { HttpContext } from '@adonisjs/core/http'
+import { inject } from '@adonisjs/core'
+import PasswordResetService from '#services/password_reset_service'
 
+@inject()
 export default class ProfileController {
+  constructor(private passwordResetService: PasswordResetService) {}
+
   async show({ inertia, params, auth, response }: HttpContext) {
     const user = await this.getUserWithRole(params.id)
 
@@ -16,10 +21,6 @@ export default class ProfileController {
     })
   }
 
-  private async getUserWithRole(id: number): Promise<User> {
-    return User.query().where('id', id).preload('role').firstOrFail()
-  }
-
   async update({ request, response, session }: HttpContext) {
     const { id } = request.params()
     const user = await User.findOrFail(id)
@@ -31,5 +32,24 @@ export default class ProfileController {
 
     session.flash('success', 'Profile updated successfully')
     return response.redirect().back()
+  }
+
+  async requestPasswordReset({ params, response, session }: HttpContext) {
+    const user = await this.getUserWithRole(params.id)
+
+    try {
+      const resetToken = await user.initiatePasswordReset()
+      await this.passwordResetService.sendResetEmail(user, resetToken.token)
+      session.flash('info', 'Password reset instructions have been sent to your email.')
+    } catch (error) {
+      console.error('Error requesting password reset:', error)
+      session.flash('error', 'An error occurred while requesting password reset. Please try again.')
+    }
+
+    return response.redirect().back()
+  }
+
+  private async getUserWithRole(id: number): Promise<User> {
+    return User.query().where('id', id).preload('role').firstOrFail()
   }
 }
