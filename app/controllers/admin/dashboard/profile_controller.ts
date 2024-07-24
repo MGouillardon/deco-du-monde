d85a2c0e73c3a433846b353bd2d7bcf5f41cfce0
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2024-07-24 11:28:23 +0200

    feat: load user's role to show method

--- app/controllers/admin/dashboard/profile_controller.ts
+++ app/controllers/admin/dashboard/profile_controller.ts
@@ -3,14 +3,20 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ProfileController {
   async show({ inertia, params, auth, response }: HttpContext) {
-    const title = 'Profile'
-    const user = await User.findOrFail(params.id)
+    const user = await this.getUserWithRole(params.id)
 
     if (auth.user!.id !== user.id) {
-      return response.redirect().back()
+      return response.forbidden('You are not authorized to view this profile')
     }
 
-    return inertia.render('Admin/Dashboard/Profile/Show', { user, title })
+    return inertia.render('Admin/Dashboard/Profile/Show', {
+      title: 'Profile',
+      user: user.serialize(),
+    })
+  }
+
+  private async getUserWithRole(id: number): Promise<User> {
+    return User.query().where('id', id).preload('role').firstOrFail()
   }
 
   async update() {}
