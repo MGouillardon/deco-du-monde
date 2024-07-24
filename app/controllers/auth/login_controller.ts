a619f724369bd47db8243bc634b7d6b0ed19b66f
Maxime <maxime.gouillardon@gmail.com>
2024-07-24 19:18:12 +0200

    refactor: add services to controlles login & password reset

--- app/controllers/auth/login_controller.ts
+++ app/controllers/auth/login_controller.ts
@@ -1,13 +1,11 @@
-import User from '#models/user'
-import { loginValidator } from '#validators/auth/login'
 import type { HttpContext } from '@adonisjs/core/http'
-import { errors } from '@adonisjs/auth'
-import EmailService from '#services/email_service'
+import { loginValidator } from '#validators/auth/login'
+import AuthService from '#services/auth_service'
 import { inject } from '@adonisjs/core'
 
 @inject()
 export default class LoginController {
-  constructor(private emailService: EmailService) {}
+  constructor(private authService: AuthService) {}
 
   async render({ inertia }: HttpContext) {
     return inertia.render('Admin/Auth/Login')
@@ -17,29 +15,20 @@ export default class LoginController {
     const { email, password } = await request.validateUsing(loginValidator)
 
     try {
-      const user = await User.verifyCredentials(email, password)
+      const user = await this.authService.verifyCredentials(email, password)
 
       if (!user.isPasswordChanged) {
-        const resetToken = await user.initiatePasswordReset()
-        await this.emailService.sendPasswordResetEmail(user, resetToken.token)
+        await this.authService.handlePasswordReset(user)
         session.flash('info', 'Please check your email to reset your password.')
         return response.redirect().back()
       }
 
-      await auth.use('web').login(user)
+      await this.authService.login(auth, user)
       session.flash('success', 'You have successfully logged in.')
       return response.redirect().toRoute('dashboard')
     } catch (error) {
-      this.handleLoginError(error, session)
+      session.flash('errors', error.message)
       return response.redirect().back()
-    }
-  }
-
-  private handleLoginError(error: unknown, session: HttpContext['session']) {
-    if (error instanceof errors.E_INVALID_CREDENTIALS) {
-      session.flash('errors', 'Invalid email or password')
-    } else {
-      session.flash('errors', 'An unexpected error occurred. Please try again.')
     }
   }
 }
--- app/controllers/auth/password_reset_controller.ts
+++ app/controllers/auth/password_reset_controller.ts
@@ -1,11 +1,19 @@
+import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
-import PasswordResetToken from '#models/password_reset_token'
 import { resetPasswordValidator } from '#validators/auth/reset_password'
-import User from '#models/user'
+import PasswordResetService from '#services/password_reset_service'
+import AuthService from '#services/auth_service'
 
+@inject()
 export default class PasswordResetController {
+  constructor(
+    private passwordResetService: PasswordResetService,
+    private authService: AuthService
+  ) {}
+
   async show({ params, inertia }: HttpContext) {
     const { token } = params
+    console.log('Token:', token)
     return inertia.render('Admin/Auth/ResetPassword', { token })
   }
 
@@ -13,25 +21,14 @@ export default class PasswordResetController {
     const { token, password } = await request.validateUsing(resetPasswordValidator)
 
     try {
-      const resetToken = await PasswordResetToken.findValidToken(token)
-      if (!resetToken) {
-        session.flash('errors', 'Invalid or expired token')
-        return response.redirect().back()
-      }
+      const user = await this.passwordResetService.resetPassword(token, password)
+      await this.authService.login(auth, user)
 
-      const user = await User.findOrFail(resetToken.userId)
-      user.password = password
-      user.isPasswordChanged = true
-      await user.save()
-
-      await resetToken.delete()
-
-      await auth.use('web').login(user)
       session.flash('success', 'Password reset successfully')
       return response.redirect().toRoute('dashboard')
     } catch (error) {
       console.error('Error in reset function:', error)
-      session.flash('errors', 'An error occurred during password reset')
+      session.flash('errors', error.message || 'An error occurred during password reset')
       return response.redirect().back()
     }
   }
