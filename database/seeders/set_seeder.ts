c7e1d068ceaf45487ed933c891b256da91b640d9
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2024-07-31 15:26:57 +0200

    fix: delete set seeder ability to create item

--- database/seeders/set_seeder.ts
+++ database/seeders/set_seeder.ts
@@ -1,13 +1,8 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import { SetFactory } from '#factories/set_factory'
-import { DateTime } from 'luxon'
 
 export default class SetSeeder extends BaseSeeder {
   async run() {
-    await SetFactory.with('items', Math.floor(Math.random() * 8) + 3, (item) => {
-      item.pivotAttributes(() => ({
-        created_at: DateTime.local().toSQL({ includeOffset: false }),
-      }))
-    }).createMany(10)
+    await SetFactory.createMany(10)
   }
 }
