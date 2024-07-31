86df08de6c526d017449c8e30e043ce8cf7e560f
Maxime <maxime.gouillardon@gmail.com>
2024-07-31 07:45:05 +0200

    feat: get status & set related to item in index method - item controller

--- app/controllers/admin/dashboard/item_controller.ts
+++ app/controllers/admin/dashboard/item_controller.ts
@@ -2,9 +2,14 @@ import Item from '#models/item'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ItemController {
-  async index({ inertia }: HttpContext) {
+  async index({ request, inertia }: HttpContext) {
     const title = 'Listing items'
-    const items = await Item.all()
+    const page = request.input('page', 1)
+    const limit = 10
+    const items = await Item.query()
+      .preload('status', (query) => query.select('status'))
+      .preload('sets', (query) => query.select('id', 'name', 'is_photographed'))
+      .paginate(page, limit)
     return inertia.render('Admin/Dashboard/Items/Index', { title, items })
   }
 
