ec42ad1f5986e69231c427a0c0dfd431b295f194
Maxime <maxime.gouillardon@gmail.com>
2024-08-21 16:53:22 +0200

    fix: fix item validation seeder to be more accurate with my logic, only photographied items can be validated

--- database/seeders/item_validation_seeder.ts
+++ database/seeders/item_validation_seeder.ts
@@ -3,31 +3,37 @@ import Item from '#models/item'
 import User from '#models/user'
 import ItemValidation from '#models/item_validation'
 import { DateTime } from 'luxon'
+import { ScheduleType } from '#enums/schedule_type'
 
 export default class ItemValidationSeeder extends BaseSeeder {
   async run() {
-    const items = await Item.query()
-      .where('isPhotographedStudio', true)
-      .orWhereHas('sets', () => {})
-
     const photographers = await User.query().whereHas('role', (query) => {
       query.where('name', 'photograph').orWhere('name', 'assistant_photograph')
     })
 
-    for (const item of items) {
+    const studioItems = await Item.query().where('isPhotographedStudio', true)
+
+    const photographedSetItems = await Item.query().whereHas('sets', (setQuery) => {
+      setQuery.whereHas('schedules', (scheduleQuery) => {
+        scheduleQuery
+          .where('type', ScheduleType.SET_SHOOT)
+          .where('start_time', '<=', DateTime.now().toSQL())
+      })
+    })
+
+    const allPhotographedItems = [...studioItems, ...photographedSetItems]
+
+    for (const item of allPhotographedItems) {
       const randomPhotographer = photographers[Math.floor(Math.random() * photographers.length)]
       const isValidated = Math.random() < 0.75
-
-      const now = DateTime.now()
-
-      await ItemValidation.updateOrCreate(
-        { itemId: item.id },
-        {
-          userId: isValidated ? randomPhotographer.id : null,
-          isValidated: isValidated,
-          validatedAt: isValidated ? now : null,
-        }
-      )
+      if (isValidated) {
+        await ItemValidation.create({
+          itemId: item.id,
+          userId: randomPhotographer.id,
+          isValidated: true,
+          validatedAt: DateTime.now(),
+        })
+      }
     }
   }
 }
