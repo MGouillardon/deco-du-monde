fdda34387020a12bbcc0cab1a0ec7942095d98af
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2024-11-08 17:00:37 +0100

    refactor: add other status to enum item status

--- app/enums/item_status.ts
+++ app/enums/item_status.ts
@@ -2,4 +2,10 @@ export enum ItemStatusType {
   NORMAL = 'normal',
   DAMAGED = 'damaged',
   LOST = 'lost',
+  STUDIO_SCHEDULED = 'studio_scheduled',
+  STUDIO_COMPLETED = 'studio_completed',
+  STUDIO_VALIDATED = 'studio_validated',
+  SET_SCHEDULED = 'set_scheduled',
+  SET_COMPLETED = 'set_completed',
+  SET_VALIDATED = 'set_validated',
 }
