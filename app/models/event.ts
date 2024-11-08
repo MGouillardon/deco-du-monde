4cb344a9db173cd02ab70f0e16daeaf792c0bb80
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2024-11-08 17:00:07 +0100

    feat(event_model): add complete an event logic with item status manipulation

--- app/models/event.ts
+++ app/models/event.ts
@@ -7,6 +7,7 @@ import EventAssignment from '#models/event_assignment'
 import Workday from '#models/workday'
 import Set from '#models/set'
 import { EventType } from '#enums/event_type'
+import { ItemStatusType } from '#enums/item_status'
 import Item from '#models/item'
 import string from '@adonisjs/core/helpers/string'
 
@@ -32,6 +33,12 @@ export default class Event extends BaseModel {
   @column()
   declare setId: number | null
 
+  @column()
+  declare completed: boolean
+
+  @column.dateTime()
+  declare completedAt: DateTime | null
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
@@ -52,6 +59,40 @@ export default class Event extends BaseModel {
 
   @belongsTo(() => Workday)
   declare workday: BelongsTo<typeof Workday>
+
+  async complete(trx?: any) {
+    this.completed = true
+    this.completedAt = DateTime.now()
+    await this.save()
+
+    if (this.itemId) {
+      const item = await Item.findOrFail(this.itemId)
+      await this.updateItemStatus(item, trx)
+    }
+  }
+
+  private async updateItemStatus(item: Item, trx?: any) {
+    const newStatus = this.determineNewItemStatus()
+    if (newStatus) {
+      await item.related('itemStatus').create(
+        {
+          status: newStatus,
+        },
+        trx ? { client: trx } : undefined
+      )
+    }
+  }
+
+  private determineNewItemStatus(): ItemStatusType | null {
+    switch (this.type) {
+      case EventType.STUDIO_SHOOT:
+        return ItemStatusType.STUDIO_COMPLETED
+      case EventType.SET_SHOOT:
+        return ItemStatusType.SET_COMPLETED
+      default:
+        return null
+    }
+  }
 
   @computed()
   get title() {
