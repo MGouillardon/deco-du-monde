5ca3ae2c00bbd98ae5d9f3c30d0810a18b1154d8
Maxime <maxime.gouillardon@gmail.com>
2024-12-05 22:35:29 +0100

    fix(policies): resolve TypeScript issues in UserPolicy

--- app/policies/user_policy.ts
+++ app/policies/user_policy.ts
@@ -8,4 +8,20 @@ export default class UserPolicy extends BasePolicy {
     if (!user) return false
     return user.roleId === Roles.ADMIN
   }
+
+  viewAny(_user: User): AuthorizerResponse {
+    return false
+  }
+  view(_user: User): AuthorizerResponse {
+    return false
+  }
+  create(_user: User): AuthorizerResponse {
+    return false
+  }
+  update(_user: User): AuthorizerResponse {
+    return false
+  }
+  delete(_user: User): AuthorizerResponse {
+    return false
+  }
 }
