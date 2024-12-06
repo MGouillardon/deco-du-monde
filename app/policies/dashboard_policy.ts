b60019c68099ed5ecef32d1885f5a7e1100e35c2
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2024-12-06 11:29:14 +0100

    refactor: restrict dashboard access to specific roles

--- app/policies/dashboard_policy.ts
+++ app/policies/dashboard_policy.ts
@@ -1,9 +1,10 @@
 import { BasePolicy } from '@adonisjs/bouncer'
 import User from '#models/user'
 import type { AuthorizerResponse } from '@adonisjs/bouncer/types'
+import Roles from '#enums/roles'
 
 export default class DashboardPolicy extends BasePolicy {
-  viewDashboard(_user: User): AuthorizerResponse {
-    return true
+  viewDashboard(user: User): AuthorizerResponse {
+    return [Roles.ADMIN, Roles.PHOTOGRAPH, Roles.DECORATOR].includes(user.roleId)
   }
 }
