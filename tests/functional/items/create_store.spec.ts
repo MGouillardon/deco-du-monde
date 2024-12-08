50e574b1c19cb9867fc70d0b3a2bcbc9ec44c38d
Maxime <maxime.gouillardon@gmail.com>
2024-12-08 16:47:47 +0100

    test(items): implement create and store endpoints tests

---
+++ tests/functional/items/create_store.spec.ts
@@ -0,0 +1,107 @@
+import { test } from '@japa/runner'
+import { createAdminUser, createTestUser } from '../users/helpers/setup.js'
+import { testItemData } from './helpers/setup.js'
+import Roles from '#enums/roles'
+import { ItemStatusType } from '#enums/item_status'
+import { LocationType } from '#enums/location_type'
+import Item from '#models/item'
+import Database from '@adonisjs/lucid/services/db'
+
+test.group('Items create/store', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('admin can access create form', async ({ client, route }) => {
+    const admin = await createAdminUser()
+
+    const response = await client.get(route('create.item')).withInertia().loginAs(admin)
+
+    response.assertStatus(200)
+    response.assertInertiaComponent('Admin/Dashboard/Items/Create')
+    response.assertInertiaPropsContains({
+      title: 'Create item',
+      statusOptions: Object.values(ItemStatusType),
+    })
+  })
+
+  test('photographer can access create form', async ({ client, route }) => {
+    const photographer = await createTestUser(Roles.PHOTOGRAPH)
+
+    const response = await client.get(route('create.item')).withInertia().loginAs(photographer)
+
+    response.assertStatus(200)
+    response.assertInertiaComponent('Admin/Dashboard/Items/Create')
+  })
+
+  test('decorator cannot access create form', async ({ client, route }) => {
+    const decorator = await createTestUser(Roles.DECORATOR)
+
+    const response = await client.get(route('create.item')).withInertia().loginAs(decorator)
+
+    response.assertStatus(403)
+  })
+
+  test('admin can create new item', async ({ client, route, assert }) => {
+    const admin = await createAdminUser()
+
+    const response = await client
+      .post(route('store.item'))
+      .withCsrfToken()
+      .withInertia()
+      .loginAs(admin)
+      .form(testItemData)
+
+    response.assertStatus(200)
+    response.assertRedirectsTo(route('listing.item'))
+    response.assertInertiaPropsContains({
+      success: 'Item created successfully',
+    })
+    const item = await Item.query()
+      .preload('itemStatus')
+      .preload('validations')
+      .orderBy('id', 'desc')
+      .firstOrFail()
+
+    assert.equal(item.name, testItemData.name)
+    assert.equal(item.description, testItemData.description)
+    assert.equal(item.isPhotographedStudio, testItemData.isPhotographedStudio)
+
+    assert.equal(item.itemStatus.status, ItemStatusType.NORMAL)
+
+    assert.lengthOf(item.validations, 1)
+    assert.equal(item.validations[0].type, LocationType.STUDIO)
+    assert.equal(item.validations[0].isValidated, false)
+  })
+
+  test('photographer can create new item', async ({ client, route }) => {
+    const photographer = await createTestUser(Roles.PHOTOGRAPH)
+
+    const response = await client
+      .post(route('store.item'))
+      .withCsrfToken()
+      .withInertia()
+      .loginAs(photographer)
+      .form(testItemData)
+
+    response.assertStatus(200)
+    response.assertRedirectsTo(route('listing.item'))
+    response.assertInertiaPropsContains({
+      success: 'Item created successfully',
+    })
+  })
+
+  test('decorator cannot create new item', async ({ client, route }) => {
+    const decorator = await createTestUser(Roles.DECORATOR)
+
+    const response = await client
+      .post(route('store.item'))
+      .withCsrfToken()
+      .withInertia()
+      .loginAs(decorator)
+      .form(testItemData)
+
+    response.assertStatus(403)
+  })
+})
