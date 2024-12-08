69c7548fe35b51fa461aa697b1d3c67822c184dc
Maxime <maxime.gouillardon@gmail.com>
2024-12-08 10:56:01 +0100

    fix(tests): replace db truncate with migrate to maintain referential integrity

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -35,7 +35,7 @@ export const plugins: Config['plugins'] = [
  * The teardown functions are executer after all the tests
  */
 export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
-  setup: [() => testUtils.db().truncate(), () => testUtils.db().seed()],
+  setup: [() => testUtils.db().migrate(), () => testUtils.db().seed()],
   teardown: [],
 }
 
