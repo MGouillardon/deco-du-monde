19e581120b97a494924cae7b9afbed0cfddeabb3
Maxime <maxime.gouillardon@gmail.com>
2024-12-08 18:17:31 +0100

    refactor(item_status): remove some status to enum

--- app/enums/item_status.ts
+++ app/enums/item_status.ts
@@ -2,10 +2,4 @@ export enum ItemStatusType {
   NORMAL = 'normal',
   DAMAGED = 'damaged',
   LOST = 'lost',
-  STUDIO_SCHEDULED = 'studio_scheduled',
-  STUDIO_COMPLETED = 'studio_completed',
-  STUDIO_VALIDATED = 'studio_validated',
-  SET_SCHEDULED = 'set_scheduled',
-  SET_COMPLETED = 'set_completed',
-  SET_VALIDATED = 'set_validated',
 }
